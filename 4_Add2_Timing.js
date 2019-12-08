438c4aba3de79b10da33d2ddc51f9cb5eff29c04
Halil Erkan Dursunoğlu <h.erkan.dursunoglu@gmail.com>
2019-12-08 12:39:11 +0300

    initial commit

---
+++ 4_Add2_Timing.js
@@ -0,0 +1,13 @@
+const { performance } = require('perf_hooks');
+
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+// Different machines and the same machine
+// will record different times.
